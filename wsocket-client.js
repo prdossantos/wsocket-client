2fde17d89e0050c6b25ccca63e05bfe2574c2743
prdossantos <paulo.ricardo0217@gmail.com>
2016-08-05 16:50:17 -0300

    Initial commit

--- wsocket-client.js
+++ wsocket-client.js
@@ -1,40 +1,33 @@
-var WSC;
-(function(window,document){
-	WSC = function(url,protocol)
-	{
-		function _set()
-		{
+var WSC = {
+	socket: null,
+	connect: (url,protocol) => {
+		if(url)
+			this.socket = new WebSocket(url,protocol);	
+	},
+ 	
+	open: ( event, callback ) => {
+		this.socket.onopen = ( callback ) ? callback(event) : this._promise(event)
+	},
 
-		}
+	send: (data, callback ) => {
+		console.log(this.socket)
+		this.socket.send(data) 
+		if( callback ) 
+			callback(event) 
+		else 
+			this._promise(event)
+ 	},
 
-		function _get()
-		{
+	receive: ( event, callback ) => {
+		this.socket.onmessage = ( callback ) ? callback(event) : this._promise(event)
+ 	},
 
-		}
-
-		function _on( command, data, callback )
-		{
-
-			if( callback )
-				callback(data)
-			else
-				_promise(data)
-		}
-
-		function _promise( res, rej )
-		{
-			return new Promise( function( resolve, reject ) {
-				if( res )
-					resolve(res)
-				if( rej )
-					reject(rej)
-			})
-		}
-
-		return {
-			set: _set,
-			get: _get,
-			on: _on
-		}
-	}
-});
+    _promise: ( res, rej ) => {
+		return new Promise( function( resolve, reject ) {
+			if( res )
+				resolve(res)
+			if( rej )
+				reject(rej)
+		})
+	},
+};
